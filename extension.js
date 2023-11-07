8a4ad2d1b124b7a3cb19880562c3da7eff88a310
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-11-07 14:52:41 +0800

    Add ESLint configuration and fix linting issues

--- extension.js
+++ extension.js
@@ -1,25 +1,25 @@
-const vscode = require('vscode');
+const vscode = require('vscode')
 
 function activate(context) {
-	let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, Number.MIN_VALUE);
+  const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, Number.MIN_VALUE)
 
-	statusBarItem.text = `$(terminal-tmux)`;
-	statusBarItem.tooltip = 'Merge All Window Tabs';
-	statusBarItem.command = 'monowindow.mergeAllWindowTabs';
+  statusBarItem.text = `$(terminal-tmux)`
+  statusBarItem.tooltip = 'Merge All Window Tabs'
+  statusBarItem.command = 'monowindow.mergeAllWindowTabs'
 
-	statusBarItem.show();
+  statusBarItem.show()
 
-	let disposable = vscode.commands.registerCommand('monowindow.mergeAllWindowTabs', function () {
-		vscode.commands.executeCommand('workbench.action.mergeAllWindowTabs');
-	});
+  const disposable = vscode.commands.registerCommand('monowindow.mergeAllWindowTabs', () => {
+    vscode.commands.executeCommand('workbench.action.mergeAllWindowTabs')
+  })
 
-	context.subscriptions.push(statusBarItem);
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(statusBarItem)
+  context.subscriptions.push(disposable)
 }
 
 function deactivate() { }
 
 module.exports = {
-	activate,
-	deactivate
-};
+  activate,
+  deactivate,
+}
