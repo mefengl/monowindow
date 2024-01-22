17d0e36ce1bc744e313019e6423a6def8f106b04
冯不游 <71683364+mefengl@users.noreply.github.com>
2024-01-22 19:10:28 +0800

    Add merge and auto monowindow functionality
    Add merge and auto monowindow functionality to the extension.js file. Update package.json with new commands and configuration options.

--- extension.js
+++ extension.js
@@ -1,23 +1,75 @@
 const vscode = require('vscode')
 
-function activate(context) {
-  const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, Number.MIN_VALUE)
+let autoMonowindowInterval = null
+let mergeStatusItem
+let autoMonowindowStatusItem
 
-  statusBarItem.text = `$(terminal-tmux)`
-  statusBarItem.tooltip = 'Merge All Window Tabs'
-  statusBarItem.command = 'monowindow.mergeAllWindowTabs'
-
-  statusBarItem.show()
-
-  const disposable = vscode.commands.registerCommand('monowindow.mergeAllWindowTabs', () => {
-    vscode.commands.executeCommand('workbench.action.mergeAllWindowTabs')
-  })
-
-  context.subscriptions.push(statusBarItem)
-  context.subscriptions.push(disposable)
+function updateStatusItem() {
+  if (autoMonowindowInterval) {
+    autoMonowindowStatusItem.text = `$(check) $(terminal-tmux)`
+    autoMonowindowStatusItem.tooltip = 'Auto MonoWindow is ON. Click to turn off'
+  } else {
+    autoMonowindowStatusItem.text = `$(x) $(terminal-tmux)`
+    autoMonowindowStatusItem.tooltip = 'Auto MonoWindow is OFF. Click to turn on'
+  }
 }
 
-function deactivate() { }
+function showOrHideStatusItems() {
+  if (vscode.workspace.getConfiguration().get('monowindow.showMergeButton')) {
+    mergeStatusItem.show()
+  } else {
+    mergeStatusItem.hide()
+  }
+
+  if (vscode.workspace.getConfiguration().get('monowindow.showToggleAutoButton')) {
+    autoMonowindowStatusItem.show()
+  } else {
+    autoMonowindowStatusItem.hide()
+  }
+}
+
+function activate(context) {
+  mergeStatusItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, Number.MIN_VALUE)
+  mergeStatusItem.text = `$(terminal-tmux)`
+  mergeStatusItem.tooltip = 'Merge All Window Tabs'
+  mergeStatusItem.command = 'monowindow.mergeAllWindowTabs'
+
+  autoMonowindowStatusItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, Number.MIN_VALUE)
+  autoMonowindowStatusItem.command = 'monowindow.toggleAutoMonowindow'
+  updateStatusItem()
+
+  showOrHideStatusItems()
+
+  vscode.workspace.onDidChangeConfiguration(() => {
+    showOrHideStatusItems()
+  });
+
+  const disposableMerge = vscode.commands.registerCommand('monowindow.mergeAllWindowTabs', () => {
+    vscode.commands.executeCommand('workbench.action.mergeAllWindowTabs')
+  });
+
+  const disposableToggle = vscode.commands.registerCommand('monowindow.toggleAutoMonowindow', () => {
+    if (autoMonowindowInterval) {
+      clearInterval(autoMonowindowInterval)
+      autoMonowindowInterval = null
+      vscode.window.showInformationMessage('Auto MonoWindow deactivated.')
+    } else {
+      autoMonowindowInterval = setInterval(() => {
+        vscode.commands.executeCommand('workbench.action.mergeAllWindowTabs')
+      }, 1000);
+      vscode.window.showInformationMessage('Auto MonoWindow activated.')
+    }
+    updateStatusItem();
+  });
+
+  context.subscriptions.push(mergeStatusItem, disposableMerge, disposableToggle, autoMonowindowStatusItem)
+}
+
+function deactivate() {
+  if (autoMonowindowInterval) {
+    clearInterval(autoMonowindowInterval)
+  }
+}
 
 module.exports = {
   activate,
